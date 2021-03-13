3e845ec1e69414510000cbd66cbd0e6ef2dd4f03
Supritha <78655439+supzi-del@users.noreply.github.com>
2021-03-13 23:33:14 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,78 +1,87 @@
-document.querySelector("#addnew").addEventListener('click',function(){
-    document.querySelector(".clickbox").style.display="flex";
-    
-});
+const container = document.querySelector('.container');
+var inputValue = document.querySelector('.input');
+const add = document.querySelector('.add');
 
-document.querySelector("#close").addEventListener('click',function(){
-    document.querySelector(".clickbox").style.display="none";
-});
+if(window.localStorage.getItem("urls") == undefined){
+     var urls = [];
+     window.localStorage.setItem("urls", JSON.stringify(urls));
+}
+
+var urlsEX = window.localStorage.getItem("urls");
+var urls = JSON.parse(urlsEX);
 
 
+class item{
+	constructor(name){
+		this.createItem(name);
+	}
+    createItem(name){
+    	var itemBox = document.createElement('div');
+        itemBox.classList.add('item');
 
+    	var input = document.createElement('input');
+    	input.type = "text";
+    	input.disabled = true;
+    	input.value = name;
+    	input.classList.add('item_input');
 
-var myArr=[];
-    document.querySelector("#addurl").addEventListener('click',function(){
-        
-        
-        
-        var pastedurl = document.getElementById("linkk").value;
-        
-        
-        
-        myArr.push(pastedurl);
-        
-        
-        var pval="";
+    	var edit = document.createElement('button');
+    	edit.classList.add('edit');
+    	edit.innerHTML = "EDIT";
+    	edit.addEventListener('click', () => this.edit(input, name));
 
-        for(let i=0;i< myArr.length;i++){
-            pval=pval + myArr[i]+ "<br/>";
+    	var remove = document.createElement('button');
+    	remove.classList.add('remove');
+    	remove.innerHTML = "REMOVE";
+    	remove.addEventListener('click', () => this.remove(itemBox, name));
 
-            document.querySelector(".row").style.display="flex";
-            
-            document.querySelector(".row").innerHTML= pval;
-            
-            
-            //document.querySelector(".row").style.overflow= "";
-            document.querySelector(".clickbox").style.display="none";
+    	container.appendChild(itemBox);
 
-            
+        itemBox.appendChild(input);
+        itemBox.appendChild(edit);
+        itemBox.appendChild(remove);
+
+    }
+
+    edit(input, name){
+        if(input.disabled == true){
+           input.disabled = !input.disabled;
         }
+    	else{
+            input.disabled = !input.disabled;
+            let indexof = urls.indexOf(name);
+            urls[indexof] = input.value;
+            window.localStorage.setItem("urls", JSON.stringify(urls));
+        }
+    }
 
-        
+    remove(itemBox, name){
+        itemBox.parentNode.removeChild(itemBox);
+        let index = urls.indexOf(name);
+        urls.splice(index, 1);
+        window.localStorage.setItem("urls", JSON.stringify(urls));
+    }
+}
 
-        
-       
+add.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+	if(e.which == 13){
+		check();
+	}
+})
 
-       
-
-
-    });
-
-    
-
-
-
-    document.getElementById("row").addEventListener('click',function(){
-        
-      document.execCommand("copy");
-
-      document.querySelector(".copied").style.display="flex";
-
-       setTimeout(function(){
-       document.querySelector(".copied").style.display="";
-        }, 1500);
-        
-
-
-    });
+function check(){
+	if(inputValue.value != ""){
+		new item(inputValue.value);
+        urls.push(inputValue.value);
+        window.localStorage.setItem("urls", JSON.stringify(urls));
+		inputValue.value = "";
+	}
+}
 
 
+for (var v = 0 ; v < urls.length ; v++){
+    new item(urls[v]);
+}
 
-    document.getElementById("row").addEventListener('dblclick',function(){
-        
-        document.querySelector("#row").style.display="";
-        
-          
-  
-  
-      });
+
